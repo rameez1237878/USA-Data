b1ca13a038e9a75efad4255df0630e8c534521a3
rameez1237878 <rameezjutt503@gmail.com>
2025-01-14 16:39:20 +0500

    change the api coming fron .env

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect, useState,  } from 'react';
   const [myData, setMYData] = useState([]);
   const [weData, setWeData] = useState([]);
   const fetchData= async()=>{
-    let res = await axios.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+    let res = await axios.get(process.env.REACT_APP_API);
     setMYData(res.data.data);
     setWeData(res.data.source);
     //console.log(res.data.source[0].annotations, "tyhfghfgh");
@@ -17,16 +17,16 @@ import { useEffect, useState,  } from 'react';
 
   return (
     <div className='main'>
-      <h1>USA Data</h1>
+      <h1>{process.env.REACT_APP_FIRSTQ}</h1>
     <div className='mains'>
       {myData.map((data, index)=>{
           const {Nation, Population, Year} = data;
           return (
             <>
               <div className = 'card' key={index}>
-                 <div className='close'> <h3>Nation : </h3> <h4>{Nation}</h4> </div>
-                 <div className='close'> <h3>Year : </h3> <h4>{Year}</h4> </div>
-                 <div className='close'> <h3>Population :  </h3> <h4>{Population}</h4> </div>
+                 <div className='close'> <h3>{process.env.REACT_APP_NATION} </h3> <h4>{Nation}</h4> </div>
+                 <div className='close'> <h3>{process.env.REACT_APP_YEAR} </h3> <h4>{Year}</h4> </div>
+                 <div className='close'> <h3>{process.env.REACT_APP_POPULATION}</h3> <h4>{Population}</h4> </div>
               </div>
             </>
           )
@@ -34,20 +34,20 @@ import { useEffect, useState,  } from 'react';
     </div>
     {/* second */}
     <div className='main'>
-      <h1>USA Source</h1>
+      <h1>{process.env.REACT_APP_FIRSTE}</h1>
     <div className='mains'>
         {weData.map((datae, index) => {
           const {annotations, name} = datae;
           return (
               <div className='card1' key={index}>
-                <div className='close'> <h3>Source Name : </h3><h4>{annotations.source_name}</h4> </div>
-                <div className='close'> <h3>Name : </h3><h4>{name}</h4> </div>
-                <div className='close'> <h3>Source Description : </h3><h4>{annotations.source_description.slice(0,39)}...</h4> </div>
-                <div className='close'> <h3>Dataset Name : </h3><h4>{annotations.dataset_name} </h4> </div>
-                <div className='close'> <h3>Dataset Link : </h3> <h4> {annotations.dataset_link} </h4> </div>
-                <div className='close'> <h3>Table ID : </h3><h4>{annotations.table_id}</h4> </div>
-                <div className='close'> <h3>Topic : </h3><h4>{annotations.topic} </h4> </div>
-                <div className='close'> <h3>Subtopic : </h3><h4>{annotations.subtopic}</h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_SOURCE} </h3><h4>{annotations.source_name}</h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_NAME} </h3><h4>{name}</h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_SOURCE_DES} </h3><h4>{annotations.source_description.slice(0,39)}...</h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_DATASET} </h3><h4>{annotations.dataset_name} </h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_SOURCE_LINK} </h3> <h4> {annotations.dataset_link} </h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_SOURCE_TABLE}</h3><h4>{annotations.table_id}</h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_SOURCE_TOPIC}</h3><h4>{annotations.topic} </h4> </div>
+                <div className='close'> <h3>{process.env.REACT_APP_SOURCE_SUB}</h3><h4>{annotations.subtopic}</h4> </div>
               </div>
             );
         })}
