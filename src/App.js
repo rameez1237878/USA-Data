9438802c2428325170eb0025b8a76f7e631881ed
rameez1237878 <rameezjutt503@gmail.com>
2025-01-14 19:46:02 +0500

    add dropdwon button to select the years and nation

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ import { useEffect, useState,  } from 'react';
  const App = ()=> {
   const [myData, setMYData] = useState([]);
   const [weData, setWeData] = useState([]);
+  const [drilldown, setDrilldown] = useState('Nation');
+  const [year, setYear] = useState('');
   const fetchData= async()=>{
-    let res = await axios.get(process.env.REACT_APP_API);
+    let url = `${process.env.REACT_APP_API}=${drilldown}&measures=Population&year=${year}`;
+    let res = await axios.get(url);
     setMYData(res.data.data);
     setWeData(res.data.source);
     //console.log(res.data.source[0].annotations, "tyhfghfgh");
   }
-  useEffect(()=>{
-    fetchData();
-  },[])
+   useEffect(() => {
+    if (drilldown || year) {
+      fetchData();
+    }
+  }, [drilldown, year]);
 
   return (
     <div className='main'>
+      <div className="drops">
+      <div className='dropdown'>
+        <select name="" id="" value={drilldown} onChange={(e)=>setDrilldown(e.target.value)}>
+          <option className="red" value="Nation">Nation</option>
+          <option className="red" value="State">State</option>
+        </select>
+       </div>
+        <div className='dropdown'>
+        <select value={year} onChange={(e)=>setYear(e.target.value)}>
+          <option className="red" value="">select year</option>
+          <option  className="red" value="2013">2013</option>
+          <option className="red" value="2014">2014</option>
+          <option className="red" value="2015">2015</option>
+          <option className="red" value="2016">2016</option>
+          <option className="red" value="2017">2017</option>
+          <option className="red" value="2018">2018</option>
+          <option className="red" value="2019">2019</option>
+          <option className="red" value="2020">2020</option>
+          <option className="red" value="2021">2021</option>
+          <option className="red" value="2022">2022</option>
+        </select>
+        </div>
+      </div>
       <h1>{process.env.REACT_APP_FIRSTQ}</h1>
     <div className='mains'>
       {myData.map((data, index)=>{
@@ -30,6 +58,7 @@ import { useEffect, useState,  } from 'react';
               </div>
             </>
           )
+
       })} 
     </div>
     {/* second */}
